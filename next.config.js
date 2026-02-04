214f82c90c75d3f16c5e4d8ec1c9f1bc4fa9286c
Melek42 <kingmelek42@gmail.com>
2026-02-04 12:11:33 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-// next.config.js - SIMPLIFIED VERSION
+// next.config.js - REPLACE WITH THIS
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['firebasestorage.googleapis.com'],
+    domains: ['firebasestorage.googleapis.com', 'lh3.googleusercontent.com'],
   },
+  // Remove all webpack config for now
 }
 
 module.exports = nextConfig
